09cd0ec169810021b13d97f6c7ac31ff116bd3b0
MartinLuckerKing <114108870+MartinLuckerKing@users.noreply.github.com>
2023-10-18 22:44:37 +0200

    finish base layout and starting responsive

--- slider.js
+++ slider.js
@@ -1,43 +1,34 @@
 document.addEventListener("DOMContentLoaded", function() {
     const slides = document.querySelectorAll('.slide');
-    const prevButton = document.querySelector('.prev');
     const nextButton = document.querySelector('.next');
     const totalSlides = slides.length;
-    const radius = 150; // ajustez cette valeur pour modifier l'écart entre les images
+    const radius = 150; 
     const theta = 2 * Math.PI / totalSlides;
     let currSlide = 0;
 
-function updateCarousel() {
-    for (let [index, slide] of slides.entries()) {
-        slide.classList.remove('active'); // Retirez la classe 'active' de toutes les diapositives pour commencer
-
-        const angle = theta * (index - currSlide);
-        const xPosition = Math.sin(angle) * radius;
-        const zPosition = Math.cos(angle) * radius - radius;
-        let scaleValue = 0.75;
-
-        if (index === currSlide) {
-            scaleValue = 1;
-            slide.style.zIndex = 3; // Bring the middle slide to the front
-            slide.classList.add('active');  // Ajoutez la classe 'active' à la diapositive du milieu
-        } else {
-            slide.style.zIndex = 1; // Push other slides to the back
+    function updateCarousel() {
+        for (let [index, slide] of slides.entries()) {
+            slide.classList.remove('active');
+        
+            const angle = theta * (index - currSlide);
+            const xPosition = Math.sin(angle) * radius;
+            let scaleValue = 0.75;
+        
+            slide.style.zIndex = '1';  
+            if (index === currSlide) {
+                scaleValue = 1;
+                slide.style.zIndex = '2'; 
+                slide.classList.add('active');
+            }
+        
+            slide.style.transform = `translateX(${xPosition}px) scale(${scaleValue})`;
         }
-
-        slide.style.transform = `translateX(${xPosition}px) translateZ(${zPosition}px) scale(${scaleValue})`;
     }
-}
-
-    prevButton.addEventListener('click', () => {
-        currSlide = (currSlide - 1 + totalSlides) % totalSlides; // This ensures we loop correctly
-        updateCarousel();
-    });
-
+    
     nextButton.addEventListener('click', () => {
-        currSlide = (currSlide + 1) % totalSlides; // Looping as well
+        currSlide = (currSlide + 1) % totalSlides;
         updateCarousel();
     });
 
-    // Initial setup
     updateCarousel();
 });
